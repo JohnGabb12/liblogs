9330510d1eae1a87805c759691d6eb2f5398abc9
JohnGabb12 <johngabvalencerina@gmail.com>
2025-10-28 20:55:46 +0800

    a

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,12 @@
 const { getDefaultConfig } = require('expo/metro-config');
 
-// Revert to default Expo Metro config (no NativeWind/Tailwind)
+// Get default Expo Metro config
 const config = getDefaultConfig(__dirname);
+
+// Add support for .wasm files (needed for expo-sqlite on web)
+config.resolver.assetExts.push('wasm');
+
+// Remove 'wasm' from sourceExts if it was added there
+config.resolver.sourceExts = config.resolver.sourceExts.filter(ext => ext !== 'wasm');
 
 module.exports = config;
